9a8d9b5036a852ca468780d15db6e1720d247719
Toyam Cox <Vaelatern@voidlinux.org>
2024-08-11 23:00:04 -0400

    Problem: No content on the statuspage
    Solution: Port the sample content

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -12,6 +12,20 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type Card struct {
+	Title              string
+	Metric             string
+	IconText           string
+	ShowHeartPulseIcon bool
+	ShowServerIcon     bool
+	IsOK               bool
+	IsError            bool
+}
+
+type Payload struct {
+	Cards []Card
+}
+
 func serve_file(w http.ResponseWriter, req *http.Request) {
 	web_dir, err := fs.Sub(fsys, "web")
 	if err != nil {
@@ -32,10 +46,24 @@ func serve_template(tmpl string) func(http.ResponseWriter, *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	payload := Payload{Cards: []Card{
+		Card{Title: "Self", Metric: "0ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Self", ShowServerIcon: true, IsOK: true},
+		Card{Title: "Office1", Metric: "317ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Server", Metric: "68ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Server Responding", IsOK: true, IconText: "HTTP"},
+		Card{Title: "Office2", Metric: "334ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Office Synology", Metric: "66ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Office Synology Responding", IsOK: true, IconText: "HTTP"},
+		Card{Title: "Office3", IsError: true, ShowHeartPulseIcon: true},
+		Card{Title: "Home Synology", Metric: "41ms", IsOK: true, ShowHeartPulseIcon: true},
+		Card{Title: "Home Synology Responding", IsOK: true, IconText: "HTTP"}}}
+
 	return func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println("Serving a template")
 		// Load and parse the template file
-		t, err := template.ParseFS(web_dir, tmpl)
+		t, err := template.ParseFS(web_dir, tmpl, "definitions.tmpl")
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			log.Println("Error parsing template:", err)
@@ -44,7 +72,7 @@ func serve_template(tmpl string) func(http.ResponseWriter, *http.Request) {
 
 		fmt.Println("Serving a template ", w)
 		// Execute the template with data (if any)
-		err = t.Execute(w, nil)
+		err = t.Execute(w, payload)
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			log.Println("Error executing template:", err)
